13008e0d9a1afb159f8531bc93b1db5499a64570
Sayan Manna <sayan2003.dev@gmail.com>
2026-01-19 23:38:32 +0000

    signin and signup facility added

--- app/api/v1/login/route.ts
+++ app/api/v1/login/route.ts
@@ -1,10 +1,68 @@
 import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import prisma from "@/db";
+import jwt from "jsonwebtoken";
 
 export async function POST(req: NextRequest){
     const data = await req.json();
-    //verify data with database and return appropriate message
-    return NextResponse.json(
-        {requestState: " signin request recieved"}
-    )
+    const recievedPassword  = data.password;
+    const recievedEmail = data.email;
+    const user  = await prisma.user.findUnique({
+        where:{
+            email: recievedEmail,
+        },
+    });
+    if(!user){
+        console.log("User not found");
+        return NextResponse.json(
+            {
+                requestState: "signin request recieved",
+                message:"User not found"
+            },
+            {status: 402}
+        )
+    }else{
+        if(user.oauth){
+            console.log("No password set for this account use google");
+            return NextResponse.json(
+                {
+                    requestState: " signin request recieved",
+                    message: "No password set for this account use google"
+                },
+                {status:401}
+            )
+        }else{
+            const flag = await bcrypt.compare(
+                recievedPassword,
+                user.password
+            )
+            if(!flag){
+                console.log("Invalid credentials");
+                return NextResponse.json(
+                    {
+                        requestState: "signin request recieved",
+                        message: "Invalid credentials"
+                    },
+                    {status:401}
+                )
+            }else{
+                const token = jwt.sign(
+                    {
+                        email: user.email
+                    },
+                    process.env.JWT_SECRET!,
+                );
+                console.log("correct credentials");
+                return NextResponse.json(
+                    {
+                        requestState: "signin request recieved",
+                        message: "you are logged in",
+                        token: token
+                    },
+                    {status: 200}
+                )
+            }
+        }
+    }
 }
 
--- app/api/v1/register/route.ts
+++ app/api/v1/register/route.ts
@@ -1,12 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcrypt"
+import prisma from "@/db";
+
+
 export  async function POST(req: NextRequest){
-    const data = await req.json();
-    const password = data.password;
+    const recievedData = await req.json();
+    const password = recievedData.password;
     const hashedPassword = await bcrypt.hash(password, 10);
-    // console.log(data);
+    console.log(recievedData);
     //store data in db and return appropriate message
+    let hasStored = false;
+    try{
+        await prisma.user.create({
+            data:{
+                name: recievedData.name,
+                email: recievedData.email,
+                password: hashedPassword,
+            },
+        })
+        hasStored = true;
+    }catch(err){
+        console.log("Error is: ", err);
+    }
     return NextResponse.json(
-        {message: "register request recieved in system"}
+        {
+            message: "register request recieved in system",
+            hasStored: hasStored
+        }
     )
 }
